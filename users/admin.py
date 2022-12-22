639a3b2c2ee3cba42e9e267f42c6bdc61d4edf37
MrBerserk <mr.berserk228@mail.ru>
2022-12-22 11:38:59 +0300

    + admin panel

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'image']
+    filter_horizontal = ['groups', 'user_permissions']
+    fields = ['password', 'last_login', 'groups', ('username', 'email'), ('first_name', 'last_name'), 'image',
+              ('is_active', 'is_staff')]
+    ordering = ['-is_staff']
