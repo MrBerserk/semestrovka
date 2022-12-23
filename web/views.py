3e80028997b88fb0647090aa87790b33824cc712
MrBerserk <mr.berserk228@mail.ru>
2022-12-23 09:30:06 +0300

    + rename index to home_page

--- web/views.py
+++ web/views.py
@@ -5,7 +5,7 @@ from web.models import Game
 
 
 class GameListView(ListView):
-    template_name = 'web/index.html'
+    template_name = 'web/home_page.html'
     model = Game
     context_object_name = 'games'
     slug_field = 'id'
