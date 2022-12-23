50e558981aebf1b19f3c698f67f6c53fb8743162
MrBerserk <mr.berserk228@mail.ru>
2022-12-23 21:29:01 +0300

    + game and comment crud

--- web/urls.py
+++ web/urls.py
@@ -21,5 +21,11 @@ from . import views
 
 urlpatterns = [
     path('', views.GameListView.as_view(), name='home_page'),
-    path('add_game/', login_required(views.GameCreateView.as_view()), name='add_game')
+    path('add_game/', login_required(views.GameCreateView.as_view()), name='add_game'),
+    path('<slug:slug>/<int:id>', views.GameDetailView.as_view(), name='detail_game'),
+    path('<slug:slug>/<int:id>/delete', views.GameDeleteView.as_view(), name='update_game'),
+    path('<slug:slug>/<int:id>/edit', views.GameUpdateView.as_view(), name='delete_game'),
+    path('<slug:slug>/<int:game_id>/comment_delete/<int:id>/', views.CommentDeleteView.as_view(),
+         name='update_comment'),
+    path('<slug:slug>/<int:game_id>/comment_edit/<int:id>/', views.CommentUpdateView.as_view(), name='delete_comment'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.decorators import login_required
 from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.urls import reverse
+from django.views.generic.edit import FormMixin
 
-from web.models import Game
-from web.forms import GameForm
+from web.models import Game, Comment
+from web.forms import GameForm, CommentForm
 
 
 class GameListView(ListView):
@@ -15,8 +15,30 @@ class GameListView(ListView):
     slug_url_kwarg = 'id'
 
 
-class GameDetailView(DetailView):
-    pass
+class GameDetailView(FormMixin, DetailView):
+    template_name = 'web/'
+    form_class = GameForm
+    model = Game
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.post = self.get_object()
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('detail_game', args=(self.kwargs['slug'], self.kwargs['id']))
 
 
 class GameCreateView(CreateView):
@@ -33,8 +55,56 @@ class GameCreateView(CreateView):
 
 
 class GameDeleteView(DeleteView):
-    pass
+    template_name = 'web/game_delete.html'
+    model = Game
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def get_success_url(self):
+        return reverse('home_page')
 
 
 class GameUpdateView(UpdateView):
-    pass
+    template_name = 'web/game_edit.html'
+    model = Game
+    form_class = GameForm
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def form_valid(self, form):
+        form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('detail_game', args=(self.object.slug, self.object.id))
+
+
+class CommentUpdateView(UpdateView):
+    template_name = 'web/comment_edit.html'
+    model = Comment
+    form_class = CommentForm
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def form_valid(self, form):
+        form.instance.slug = slugify(form.instance.text, allow_unicode=True)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('detail_game', args=(self.kwargs['slug'], self.kwargs['game_id']))
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super(CommentUpdateView, self).get_context_data(**kwargs),
+            'slug': self.kwargs['slug'],
+            'game_id': self.kwargs['game_id']
+        }
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+
+    def get_success_url(self):
+        return reverse('detail_game', args=(self.object.slug, self.object.id))
